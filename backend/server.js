58a7788901ab69c5e3e3badb7b9b2d6786299e59
unknown <chmcs2026.om.dhadke@gmail.com>
2026-03-15 01:16:07 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://hospital-rec-system.onrender.com",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
